df868c52268ddd233af532ac5195aa65c0952a74
Josh Payne <jaypay0412@gmail.com>
2021-11-23 00:54:44 -0600

    Added NPC navmesh and simple movement script

---
+++ 347_final_project/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCMovement : MonoBehaviour
+{
+    public Transform goal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Currently just targets the player's position at all times
+        // Later (once the map is done) this will be running on a state machine 
+        GameObject player = GameObject.FindWithTag("Player");
+        Vector3 pos = player.transform.position;
+
+        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = pos;
+    }
+}
