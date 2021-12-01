f4fedd068aa3d7dd7ca0ede747fdfd709771d95c
Colin OKain <colinok2001@aol.com>
2021-12-01 17:59:17 -0600

    Added tasks

--- 347_final_project/Assets/Scripts/ObjectiveItems.cs
+++ 347_final_project/Assets/Scripts/ObjectiveItems.cs
@@ -6,7 +6,11 @@ public class ObjectiveItems : MonoBehaviour
 {
     
     private static ArrayList RadioSpots = new ArrayList();
+    private static ArrayList GunSpots = new ArrayList();
+    private static ArrayList KeySpots = new ArrayList();
 
+
+    private Vector3 newLoc;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,12 +25,33 @@ public class ObjectiveItems : MonoBehaviour
         if(this.gameObject.tag == "Radio")
         {
             int radioSpot = Random.Range(0,3);
-            print(radioSpot);
             RadioSpots.Add(new Vector3(25.5f, 2.395f, 180.735f));
             RadioSpots.Add(new Vector3(17.19f, 1.63f, 179.43f));
             RadioSpots.Add(new Vector3(5.23f, 1.98f, 179.43f));
-            Vector3 newLoc = (Vector3)RadioSpots[radioSpot];
+            newLoc = (Vector3)RadioSpots[radioSpot];
             this.gameObject.transform.position = newLoc;
+        }
+        else if(this.gameObject.tag == "Gun")
+        {
+            int gunSpot = Random.Range(0, 3);
+            GunSpots.Add(new Vector3(24.705f, 0.25f, 207.9099f));
+            GunSpots.Add(new Vector3(20f, 9.467f, 197.7f));
+            GunSpots.Add(new Vector3(-9.543f, 9.968f, 197.7f));
+            newLoc = (Vector3)GunSpots[gunSpot];
+            this.gameObject.transform.position = newLoc;
+        }
+        else if (this.gameObject.tag == "Key")
+        {
+            int keySpot = Random.Range(0, 2);
+            print(keySpot);
+            KeySpots.Add(new Vector3(10.238f, 1.845f, 190.998f));
+            KeySpots.Add(new Vector3(-18.886f, 2.385f, 190.998f));
+            newLoc = (Vector3)KeySpots[keySpot];
+            this.gameObject.transform.position = newLoc;
+        }
+        else if(this.gameObject.tag == "Blackmail")
+        {
+
         }
 
 
--- 347_final_project/Assets/Scripts/PlayerScript.cs
+++ 347_final_project/Assets/Scripts/PlayerScript.cs
@@ -7,24 +7,28 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     [Header("Inspector-set Values:")]
-    // public Text taskUI;
+    public Text taskUI;
     // public GameObject canvas;  // for pausing
     CharacterController controller;
 
     public float speed;
 
+    private bool canMove;
     private string task;
     private int currentTaskNum;
-    private string[] taskList = new string[] { "radio" };
+    private string[] taskList = new string[] { "radio", "weapon", "key", "leave the house", "blackmail" };
+    Vector3 movement;
+    ArrayList inventory = new ArrayList();
 
 
 
 
     void Start()
     {
+        canMove = true;
         currentTaskNum = 0;
         task = "radio";
-        // taskUI.text = "Current Task: " + task;
+        taskUI.text = "Current Task: " + task;
         speed = 4.5f;
         controller = GetComponent<CharacterController>();
         Cursor.visible = false;
@@ -34,14 +38,21 @@ public class PlayerScript : MonoBehaviour
     private void Update()
     {
 
-        // Getting keyboard from input and building a velocity vector
-        float vertical = speed * Input.GetAxis("Vertical");
-        float horizontal = speed * Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
 
-        // Transforming built velocity vector to match the direction the player is currently facing
-        movement = transform.TransformDirection(movement);
-        controller.SimpleMove(movement);    // Moving character controller
+        if (canMove)
+        {
+
+            // Getting keyboard from input and building a velocity vector
+            float vertical = speed * Input.GetAxis("Vertical");
+            float horizontal = speed * Input.GetAxis("Horizontal");
+            movement = new Vector3(horizontal, 0.0f, vertical);
+
+            // Transforming built velocity vector to match the direction the player is currently facing
+            movement = transform.TransformDirection(movement);
+            controller.SimpleMove(movement);    // Moving character controller
+
+        }
+
 
         // Rotating player character based off mouse input
         transform.Rotate(Vector3.up * Input.GetAxis("Mouse X") * 2f);
@@ -53,6 +64,12 @@ public class PlayerScript : MonoBehaviour
         transform.eulerAngles = currentRotation;
 
         print(lookingAt());
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            canMove = false;
+            Interact();
+        }
     }
 
     void FixedUpdate()
@@ -65,9 +82,15 @@ public class PlayerScript : MonoBehaviour
 
     void UpdateTask()
     {
-        // taskUI.text = "Current Task: " + taskList[currentTaskNum];
-        currentTaskNum++;
-        task = taskList[currentTaskNum];
+        taskUI.text = "Current Task: " + taskList[currentTaskNum];
+        try
+        {
+            task = taskList[currentTaskNum];
+        }
+        catch
+        {
+            print("No more tasks available");
+        };
     }
 
     GameObject lookingAt()
@@ -84,4 +107,72 @@ public class PlayerScript : MonoBehaviour
         return hit.transform.gameObject;
     }
 
+    void Interact()
+    {
+        GameObject item = lookingAt();
+        try { 
+            if (item.tag == "Radio")
+            {
+                if (taskList[currentTaskNum] == task && task == "radio")
+                {
+                    currentTaskNum++;
+                    UpdateTask();
+                }
+            }
+            else if (item.tag == "Gun")
+            {
+                if (taskList[currentTaskNum] == task && task == "weapon")
+                {
+                    currentTaskNum++;
+                    UpdateTask();
+                }
+            }
+            else if (item.tag == "Key")
+            {
+                if (taskList[currentTaskNum] == task && task == "key")
+                {
+                    Destroy(item);
+                    inventory.Add("KeyHouse1");
+                    currentTaskNum++;
+                    UpdateTask();
+                }
+            }
+            else if (item.tag == "Door")
+            {
+                if (inventory.Contains("KeyHouse1"))
+                {
+                    TeleportToHouse2();
+                    if (task != "blackmail")
+                    {
+                        currentTaskNum++;
+                        UpdateTask();
+                    }
+                }
+            }
+            else if (item.tag == "Blackmail")
+            {
+                print(task);
+
+                if (taskList[currentTaskNum] == task && task == "blackmail")
+                {
+                    Destroy(item);
+                    inventory.Add("Blackmail");
+                    currentTaskNum++;
+                    UpdateTask();
+                }
+            }
+        }
+        catch
+        {
+
+        };
+        canMove = true;
+    }
+
+    void TeleportToHouse2()
+    {
+        Vector3 test = new Vector3(-112.0118f, 5.546f, -135.415f);
+
+        this.gameObject.transform.position = test;
+    }
 }
